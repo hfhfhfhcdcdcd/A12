49ce2ee7a49874e40508e0431ad53404ce8ee717
hfhfff <dyftfboys@163.com>
2024-09-24 18:24:45 +0800

    第一版，uart_rx不是以rx_done的拉高来实现触发的而是以时间的间隔1就是tb文件里的#100来换一个数据

--- recieve.v
+++ recieve.v
@@ -3,14 +3,14 @@ module recieve (
     input rst,
     input [2:0]Baud_set,
     input uart_rx,
-    output [7:0]Data,
-    output rx_done
+    output reg [7:0]Data,
+    output reg rx_done
     );
 /*--------------------------------Uart_rx_buf--------------------------------*/
 reg [1:0]uart_rx_buf;//a buffer to store the previous clock cycle and the current clock cycle of the uart_rx signal
 always@(posedge sysclk) begin
-uart_rx_buf[0]<=uart_rx;
-uart_rx_buf[1]<=uart_rx_buf[0];
+    uart_rx_buf[0]<=uart_rx;
+    uart_rx_buf[1]<=uart_rx_buf[0];
 end
 /*--------------------------------nedge and pede--------------------------------*/
 wire pedge;//a signal to indicate the rising edge of the uart_rx signal
@@ -24,24 +24,42 @@ always @(posedge sysclk or negedge rst) begin
         Baud_16<=27;//means the freqency of 1 bit's 1/16 is 27(115200)
     end
     else case (Baud_set)
-        0:Baud_16<=651;                 //4800; 
+        0:Baud_16<=27;                 //115200; 
         1:Baud_16<=325;                  //9600; 
-        2:Baud_16<=27;                   //115200;
+        2:Baud_16<=651;                   //9600;
         default:Baud_16<=27;             //115200; 
     endcase
 end
-/*----------------------------------------En---------------------------To detect the start signal--*/
-/*-------------------------------Baud_cnt_16(from 0 to 27/2 = 13 )---------------------------------*/
+/*----------------------------------------En_rx---------------------------To detect the start signal--*/
+reg En_rx;//a signal to indicate the start of receiving
+always @(posedge sysclk or negedge rst) begin
+    if (!rst) begin
+        En_rx<=0;
+    end
+    else if (nedge) begin
+        En_rx<=1;
+    end
+    else if (rx_done) begin//What time does the reception end? When the rx_done signal is pulled high
+        En_rx<=0;
+    end
+    else
+        En_rx<=En_rx;
+end
+/*-------------------------------Baud_cnt_16(from 0 to 27 )---------------------------------*/
 reg [9:0]Baud_cnt_16;//a counter to count the number of each part of the 16 Baud_16
 always @(posedge sysclk or negedge rst) begin
     if (!rst) begin
         Baud_cnt_16<=0;
     end
-    else if (Baud_cnt_16==Baud_16-1) begin//Baud_16 is a parameter == 27
-        Baud_cnt_16<=0;
+    else if(En_rx) begin
+        if (Baud_cnt_16==Baud_16-1) begin//Baud_16 is a parameter == 27
+            Baud_cnt_16<=0;
+        end
+        else
+            Baud_cnt_16<=Baud_cnt_16+1;
     end
-    else
-        Baud_cnt_16<=Baud_cnt_16+1;
+    else//(!En_rx)   when the En_rx signal is not pulled high, means the transmission has not yet begun
+        Baud_cnt_16<=0;
 end
 /*-------------------------------cnt_16---------------------------------*/
 reg [7:0]cnt_16;//a counter to count the number of the 16 parts of the Baud_16
@@ -49,14 +67,17 @@ always @(posedge sysclk or negedge rst) begin
     if (!rst) begin
         cnt_16<=0;
     end
-    else if (Baud_cnt_16==(Baud_16/2)-1) begin
-        cnt_16<=cnt_16+1;
+    else if (En_rx) begin
+        if (Baud_cnt_16==(Baud_16/2)-1) begin
+            cnt_16<=cnt_16+1;
+        end
+        else if ((Baud_cnt_16==Baud_16-1)&&(cnt_16==159)) begin
+            cnt_16<=0;
+        end
+        else
+            cnt_16<=cnt_16;
     end
-    else if (cnt_16==159) begin
-        cnt_16<=0;
-    end
-    else
-        cnt_16<=cnt_16;
+        
 end
 /*-------------------------------r_data---------------------------------*/
 reg [2:0]r_data[7:0];//store the number of the 16 parts of the Data bit
@@ -65,8 +86,7 @@ reg [2:0]sto_bit;
 
 always @(posedge sysclk or negedge rst) begin
     if (!rst) begin
-        sta_bit<=0; 
-        sto_bit<=0; 
+        sta_bit<=0;
         r_data[0]<=0;
         r_data[1]<=0;
         r_data[2]<=0;
@@ -75,7 +95,6 @@ always @(posedge sysclk or negedge rst) begin
         r_data[5]<=0;
         r_data[6]<=0;
         r_data[7]<=0;
-        sta_bit<=0;
         sto_bit<=0;
     end
     else if (Baud_cnt_16==(Baud_16/2)-1) begin //when each 1/16's counter count to 12
@@ -93,12 +112,48 @@ always @(posedge sysclk or negedge rst) begin
             default: ;//when cnt_16 is not in the above cases, do nothing
         endcase
     end
+    else if ((Baud_cnt_16==Baud_16-1)&&(cnt_16==159)) begin
+        sta_bit<=0;
+        r_data[0]<=0;
+        r_data[1]<=0;
+        r_data[2]<=0;
+        r_data[3]<=0;
+        r_data[4]<=0;
+        r_data[5]<=0;
+        r_data[6]<=0;
+        r_data[7]<=0;
+        sto_bit<=0;
+    end
+end
 /*-------------------------------Data---------------------------------*/
 always @(posedge sysclk or negedge rst) begin
-    
+    if (!rst) begin
+        Data <= 8'd0;
+    end
+    else if(En_rx) begin//cuz the transfer is started from the r_data[0]
+        Data <= sta_bit>=4  ?1:0;
+        Data <= r_data[0]>=4?1:0;
+        Data <= r_data[1]>=4?1:0;
+        Data <= r_data[2]>=4?1:0;
+        Data <= r_data[3]>=4?1:0;
+        Data <= r_data[4]>=4?1:0;
+        Data <= r_data[5]>=4?1:0;
+        Data <= r_data[6]>=4?1:0;
+        Data <= r_data[7]>=4?1:0;
+        Data <= sto_bit>=4  ?1:0;    
+    end
+    else
+        Data <= 8'd0;
 end
 /*-------------------------------rx_done---------------------------------*/
 always @(posedge sysclk or negedge rst) begin
-    
+    if (!rst) begin
+        rx_done <= 0;
+    end
+    else if ((Baud_cnt_16==Baud_16-1)&&(cnt_16==159)) begin//when cnt_16 is 159, one byte has been received, and then rx_done is 1
+        rx_done <= 1;
+    end
+    else
+        rx_done <= 0;
 end
 endmodule
---
+++ tb.v
@@ -0,0 +1,74 @@
+`timescale 1ns/1ps
+module tb;
+reg  sysclk;
+reg  rst   ;
+reg  [2:0]Baud_set;
+reg  uart_rx;
+wire [7:0]Data;
+wire rx_done;
+
+recieve r1(
+    .sysclk  (sysclk  ),
+    .rst     (rst     ),
+    .Baud_set(Baud_set),
+    .uart_rx (uart_rx ),
+    .Data    (Data    ),
+    .rx_done (rx_done )
+);
+/*---------------------------sysclk-------------------------------*/
+initial begin
+    sysclk=0;
+end
+always #10 sysclk=~sysclk;
+/*---------------------------sysclk-------------------------------*/
+initial begin
+//0:
+rst     =0;
+Baud_set=0;
+uart_rx =8'd0;
+//1:
+#201
+rst =1; 
+#40;
+Baud_set=0;
+uart_rx=1;
+#40;
+uart_rx1(8'hAB);
+#100;       
+uart_rx1(8'hCD);
+#100;        
+uart_rx1(8'hEF);
+#100;
+$stop;
+end
+
+/*------------------------------task----------------------------------*/
+task uart_rx1;
+input [7:0]data;
+begin
+    uart_rx=1;
+    #20;
+    uart_rx=0;
+    #8680;
+    uart_rx=data[0];
+    #8680;
+    uart_rx=data[1];
+    #8680;
+    uart_rx=data[2];
+    #8680;
+    uart_rx=data[3];
+    #8680;
+    uart_rx=data[4];
+    #8680;
+    uart_rx=data[5];
+    #8680;
+    uart_rx=data[6];
+    #8680;
+    uart_rx=data[7];
+    #8680;
+    uart_rx=1;
+    #8680;
+end
+endtask
+
+endmodule
